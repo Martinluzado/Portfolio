dcaaab46c212031af03618f09d2a96d5eeb9624c
Martin Luzardo <107727637+Martinluzado@users.noreply.github.com>
2023-06-08 19:46:09 -0300

    first commit

--- src/Componentes/main.jsx
+++ src/Componentes/main.jsx
@@ -5,7 +5,7 @@ function Main() {
   return (
     <div className='mainContainer'>
         <div>
-      <h2 className='fraseMain'>This is me!</h2>
+      <h2 className='fraseMain'>About me</h2>
         <p className='pMain'>Hello, I introduce myself, I am a junior web developer "Full Stack".</p>
         <p className='pMain'>I really like this world of technology and everything that comes with it.</p>
         <p className='pMain'>I hope you like my personal website and if you want to use it as inspiration go ahead!</p>

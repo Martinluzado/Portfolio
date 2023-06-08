d35390811e6c4b3894d3ee6e814108a869b191fb
Martin Luzardo <107727637+Martinluzado@users.noreply.github.com>
2023-06-08 20:21:24 -0300

    first commit

--- src/Componentes/footer.jsx
+++ src/Componentes/footer.jsx
@@ -8,11 +8,11 @@ import {GoLogoGithub} from 'react-icons/go'
   return (
     <div className='footerContainer'>
          <div className='footerContainer2'>
-            <a href="https://www.linkedin.com/in/m4rtinluza4rdo/">
+            <a className='pFooter' href="https://www.linkedin.com/in/m4rtinluza4rdo/">
         <GrLinkedinOption style={{color:"white", fontSize:"40px", margin:"0"}}/>
              </a>
              <a href="https://github.com/Martinluzado">
-        <GoLogoGithub style={{color:"white", fontSize:"80px" ,marginTop:"15px"}}/>
+        <GoLogoGithub className='pFooter' style={{color:"white", fontSize:"80px" ,marginTop:"15px",}}/>
                 </a>
         </div>
     </div>

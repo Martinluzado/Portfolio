4b3ae7b4dca1b69b3d562c95eada7cd13a948356
Martin Luzardo <107727637+Martinluzado@users.noreply.github.com>
2023-08-31 22:30:43 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ import Footer from './Componentes/footer';
 
 function App() {
  
+  document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
+
   return (
+    
     <div className="App">
       <Header/>
       <Main/>
--- src/Componentes/header.jsx
+++ src/Componentes/header.jsx
@@ -1,31 +1,62 @@
-import React from "react";
- import { BsJournalCode } from "react-icons/bs";
- import { FiDownload } from "react-icons/fi";
- import pdfFile from '../PDFCV/CV2.pdf'
- import Logo from '../Imagenes/LogoSinFondo.png'
+import React, { useEffect, useState } from "react";
+import { BsJournalCode } from "react-icons/bs";
+import { FiDownload } from "react-icons/fi";
+import pdfFile from "../PDFCV/CV2.pdf";
+import Logo from "../Imagenes/LogoSinFondo.png";
 import "../Estilizacion/header.css";
 function Header() {
-   const handleDownloadPDF = () => {
-   const link = document.createElement('a');
-     link.href = pdfFile;
-     link.download = 'archivo.pdf'; // Nombre del archivo descargado
-     link.click();
-   };
+  const handleDownloadPDF = () => {
+    const link = document.createElement("a");
+    link.href = pdfFile;
+    link.download = "archivo.pdf"; // Nombre del archivo descargado
+    link.click();
+  };
+
+  const [leftOffset, setLeftOffset] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollY = window.scrollY;
+      const newLeftOffset = 50 * (scrollY / window.innerHeight);
+      setLeftOffset(newLeftOffset);
+    }
+
+    // Verifica el ancho de la ventana
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth >= 599) {
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    } else {
+      // Si el ancho de la ventana es menor a 600px, establece leftOffset en 0
+      setLeftOffset(0);
+    }
+  }, []);
+
   return (
     <div className="general">
-     <section className='HeaderGeneral'>
-        <div class="skewed">  <div className="tituloContainer">
-        <img className="Logo" src={Logo} alt="Logo" />
-      </div>
-      <div className="buttonContainerHeader">  
-        <a href="https://github.com/Martinluzado?tab=repositories">
+      <section className="HeaderGeneral">
+        <div class="skewed"  style={{  left: window.innerWidth >= 600 ? `calc(40% - ${leftOffset}%)` : '0',}}>
           {" "}
-          <button className="headerButton2">
-            Proyects <BsJournalCode />
-          </button>
-        </a>     
-        <button className='headerButton2' onClick={handleDownloadPDF}>Curriculum<FiDownload /></button>   
-      </div></div>
+          <div className="tituloContainer">
+            <img className="Logo" src={Logo} alt="Logo" />
+          </div>
+          <div className="buttonContainerHeader">
+            <a href="https://github.com/Martinluzado?tab=repositories">
+              {" "}
+              <button className="headerButton2">
+                Proyects <BsJournalCode />
+              </button>
+            </a>
+            <button className="headerButton2" onClick={handleDownloadPDF}>
+              Curriculum
+              <FiDownload />
+            </button>
+          </div>
+        </div>
       </section>
     </div>
   );
